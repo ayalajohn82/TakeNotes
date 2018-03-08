66f830c1a539adb7bf5735a979335dbe47dac65f
ayala <deus@ubuntu.ubuntu-domain>
2018-03-07 18:02:47 -0800

    remove unnecessary starter code

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -1,18 +1,5 @@
-/*
- * HomePage
- *
- * This is the first thing users see of our App, at the '/' route
- *
- * NOTE: while this component should technically be a stateless functional
- * component (SFC), hot reloading does not currently support SFCs. If hot
- * reloading is not a necessity for you then you can refactor it and remove
- * the linting exception.
- */
-
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
@@ -23,7 +10,7 @@ export default class HomePage extends React.PureComponent { // eslint-disable-li
           <meta name="description" content="An App that saves your Strings Homepage" />
         </Helmet>
         <h1>
-          <FormattedMessage {...messages.header} />
+          Welcome to the Homepage
         </h1>
       </div>
     );
