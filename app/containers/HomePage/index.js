05ef7026270543e2faba0089f6eb452af9d2eed8
ayala <deus@ubuntu.ubuntu-domain>
2018-03-07 18:27:53 -0800

    add input form

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
@@ -12,7 +13,17 @@ export default class HomePage extends React.PureComponent { // eslint-disable-li
         <h1>
           Welcome to the Homepage
         </h1>
+        <form onSubmit={this.props.onSubmit}>
+          <label htmlFor="add a some words">Add some Words to storage</label>
+          <input id="words" type="text" placeholder="Your words!" value={this.props.userInput} onChange={this.props.onChangeInput} />
+        </form>
       </div>
     );
   }
 }
+
+HomePage.propTypes = {
+  onSubmit: PropTypes.func,
+  userInput: PropTypes.string,
+  onChangeInput: PropTypes.func,
+};
