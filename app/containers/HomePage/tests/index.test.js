5441644f9d3b326e7a56c22198e5cd56afb429e9
ayala <deus@ubuntu.ubuntu-domain>
2018-03-09 10:04:37 -0800

    add test for home index

--- app/containers/HomePage/tests/index.test.js
+++ app/containers/HomePage/tests/index.test.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import { shallow } from 'enzyme';
+// import React from 'react';
+// import { shallow } from 'enzyme';
 
-import HomePage from '../index';
-import messages from '../messages';
+import { mapDispatchToProps } from '../index';
+import { changeInput } from '../actions';
 
 describe('<HomePage />', () => {
-  it('should render the page message', () => {
-    const renderedComponent = shallow(
-      <HomePage />
-    );
-    expect(renderedComponent.contains(
-      <FormattedMessage {...messages.header} />
-    )).toEqual(true);
+  describe('mapDispatchToProps', () => {
+    describe('onChangeInput', () => {
+      it('should be injected', () => {
+        const dispatch = jest.fn();
+        const result = mapDispatchToProps(dispatch);
+        expect(result.onChangeInput).toBeDefined();
+      });
+
+      it('should dispatch changeInput when called', () => {
+        const dispatch = jest.fn();
+        const result = mapDispatchToProps(dispatch);
+        const input = 'A NOTE';
+        result.onChangeInput({ target: { value: input } });
+        expect(dispatch).toHaveBeenCalledWith(changeInput(input));
+      });
+    });
+
+    describe('onSubmit', () => {
+      it('should be injected', () => {
+        const dispatch = jest.fn();
+        const result = mapDispatchToProps(dispatch);
+        expect(result.onSubmit).toBeDefined();
+      });
+    });
   });
 });
