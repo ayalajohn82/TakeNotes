731a9dcc1fee578a3d86406d0ff5c6ad3ba0227b
ayala <deus@ubuntu.ubuntu-domain>
2018-03-08 20:58:57 -0800

    add saga to storage

--- app/containers/Storage/saga.js
+++ app/containers/Storage/saga.js
@@ -2,11 +2,12 @@ import axios from 'axios';
 import { call, put, takeLatest } from 'redux-saga/effects';
 import { notesLoaded } from './actions';
 
-const request = () => axios.get('/retrieve');
+const request = () => axios.get('/retrieve').then((response) => response);
 
 export function* getNotes() {
   const notes = yield call(request, '/retrieve');
-  yield put(notesLoaded(notes));
+  // console.log('in saga', notes);
+  yield put(notesLoaded(notes.data));
 }
 
 export default function* notesData() {
